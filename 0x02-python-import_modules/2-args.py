28d20bae287030c7435419a63f79ab04e37467b3
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-08-25 11:41:46 -0700

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-import sys
-
-
-def main():
-    argc = len(sys.argv) - 1
-    if argc == 0:
-        print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
-    else:
-        print("{:d} arguments:".format(argc))
-        for i in range(1, argc + 1):
-            print("{}: {}".format(i, sys.argv[i]))
-
 if __name__ == "__main__":
-    main()
+    from sys import argv
+    res = ""
+    argc = len(argv) - 1
+    delim = 's:' if argc > 1 else '.' if argc == 0 else ':'
+    for count, arg in enumerate(argv[1:]):
+        res += '{:d}: {}\n'.format(count+1, arg)
+    print('{:d} argument{}\n{}'.format(argc, delim, res), end="")
