0e55623922af226158562e7756467faced83b629
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-08-25 11:54:18 -0700

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add
+
+
+def main():
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
+
+if __name__ == "__main__":
+    main()
