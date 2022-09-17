aa38800deb4e3abd54c50049a5baf59abd469ac0
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-09-16 20:44:45 -0700

    Added comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+"""
+a module Sqaure that defines a square
+"""
+
+
 class Square:
+    """
+    Definition of a Class Square:
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    You are not allowed to import any module
+    """
     def __init__(self, size):
+        """
+        initializes the square
+        """
         self.__size = size
