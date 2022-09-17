e5c0ac9b3f505e1eb2a4dd320de67b47e55ab896
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-09-16 20:41:15 -0700

    fixed bugs

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
+"""
+A square module
+"""
+
+
 class Square:
+    """ Definition of a Class Square
+        Attributes :
+                  size (int): Size of square
+    """
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        """
+        if (type(size) is not int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif (size < 0):
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
